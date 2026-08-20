9e3578ef1151c20f55ca5ce37f29ca45d54ec453
gowrav <gowrav.hassan@gmail.com>
2026-08-20 16:17:58 +0530

    HW_STARYAG3V2_H_ reversed gain to -20.0

--- hwconf/starya/g3_v2/hw_staryag3_v2.h
+++ hwconf/starya/g3_v2/hw_staryag3_v2.h
@@ -103,7 +103,7 @@
 #define VIN_R2					3240.0
 #endif
 #ifndef CURRENT_AMP_GAIN
-#define CURRENT_AMP_GAIN		20.0
+#define CURRENT_AMP_GAIN		-20.0
 #endif
 #ifndef CURRENT_SHUNT_RES
 #define CURRENT_SHUNT_RES		0.0001
